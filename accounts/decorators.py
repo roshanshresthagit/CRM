b0631ce9eecd224c31c942065c2d62b8a6e51152
roshanshresthagit <140132322+roshanshresthagit@users.noreply.github.com>
2023-07-21 08:39:06 +0545

    commit #1

---
+++ accounts/decorators.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def unauthenticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
+
+
+def allowed_user(allowed_roles=[]):
+    def decorator(view_fun):
+        def wrapper_fun(request, *args,**kwargs):
+            
+            group = None
+            if request.user.groups.exists():
+                group=request.user.groups.all()[0].name
+
+            if group in allowed_roles:
+                return view_fun(request, *args, **kwargs)
+            else:
+                return HttpResponse('You are not allowed to view this page')
+        return wrapper_fun
+    return decorator
+
+
+def admin_only(view_fun):
+    def wrapper_function(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        
+        if group == 'customer':
+            return redirect('user')
+        if group== 'admin':
+            return view_fun(request, *args, **kwargs)
+        
+    return wrapper_function
